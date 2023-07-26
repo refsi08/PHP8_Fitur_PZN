b423047d11cf3f3d323629bab57c0f91c9f5f0e3
RefDev <62316890+RefDev-00@users.noreply.github.com>
2023-07-26 09:31:41 +0700

    New string function

---
+++ NewStringFunction.php
@@ -0,0 +1,15 @@
+<?php
+
+var_dump(str_contains("Refsi Maulana", "Refsi"));
+var_dump(str_contains("Refsi Maulana", "Maulana"));
+var_dump(str_contains("Refsi Maulana", "EKo"));
+
+
+var_dump(str_starts_with("Refsi Maulana", "Refsi"));
+var_dump(str_starts_with("Refsi Maulana", "Maulana"));
+var_dump(str_starts_with("Refsi Maulana", "Eko"));
+
+
+var_dump(str_ends_with("Refsi Maulana", "Refsi"));
+var_dump(str_ends_with("Refsi Maulana", "Maulana"));
+var_dump(str_ends_with("Refsi Maulana", "Eko"));
